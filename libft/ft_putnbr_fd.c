d3127312c8aaf70ce71f7fbed7a197ee592985a9
ElijahShiro <114169882+ElijahShiro@users.noreply.github.com>
2025-10-14 19:10:14 +0200

    last functions

---
+++ libft/ft_putnbr_fd.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+#include <unistd.h>
+
+void	ft_putnbr_fd(int n , int fd)
+{
+	char	num;
+
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	
+	if(n < 0)
+	{
+		write (fd, "-", 1);
+		n *= -1;
+	}
+	if(n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	num = (n % 10) + '0';
+	write(fd, &num, 1);
+}
